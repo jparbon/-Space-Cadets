be89712da8790b17f78fdf70ee5b27f4bcc0f7bc
lavathor <shear.casey@gmail.com>
2017-05-25 20:06:32 -0500

    5/13/17 - casey's 2nd push

---
+++ ReturnToRexburg1/test/byui/cit260/returnToRexburg/control/CombatControlTest.java
@@ -0,0 +1,38 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package byui.cit260.returnToRexburg.control;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author shear
+ */
+public class CombatControlTest {
+    
+    public CombatControlTest() {
+    }
+
+    /**
+     * Test of calculateAccuracy method, of class CombatControl.
+     */
+    @Test
+    public void testCalculateAccuracy() {
+        System.out.println("calculateAccuracy");
+        int userInput = 18;
+        int baseAttackValue = 5;
+        int bonusToAttack = 5;
+        int defenseValue = 10;
+        boolean expResult = true;
+        boolean result = CombatControl.calculateAccuracy(userInput, baseAttackValue, bonusToAttack, defenseValue);
+        assertEquals(expResult, result);
+        //System.out.println("calculateAccuracy" + expResult + "/" + result);
+        // TODO review the generated test code and remove the default call to fail.
+        //fail("The test case is a prototype.");
+    }
+    
+}
