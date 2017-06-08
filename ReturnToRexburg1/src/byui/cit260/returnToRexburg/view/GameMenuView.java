3de7b9c5ba58579630f049bae681ab2508557d99
jparbon <jparbon@gmail.com>
2017-06-08 13:51:20 -0400

    6/8/17 - Jen - LocationView

--- ReturnToRexburg1/src/byui/cit260/returnToRexburg/view/GameMenuView.java
+++ ReturnToRexburg1/src/byui/cit260/returnToRexburg/view/GameMenuView.java
@@ -12,103 +12,106 @@ import java.util.Scanner;
  * @author douglasarbon1
  */
 public class GameMenuView {
-    
+
     void displayMenu() {
         //throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-    }   
-    
+    }
+
     //class instance variable
     String menu;
 
     //default constructor initialized with text to be displayed
     public GameMenuView() {
         this.menu = "\n"
-                  + "\n---------------------------------------------"
-                  + "\n| Game Menu                                 |"
-                  + "\n---------------------------------------------"
-                  + "\nA - Alien Menu"
-                  + "\nM - Return to Main Menu"
-                  + "\n---------------------------------------------";
-            
+                + "\n---------------------------------------------"
+                + "\n| Game Menu                                 |"
+                + "\n---------------------------------------------"
+                + "\nA - Alien Menu"
+                + "\nL - Location Menu"
+                + "\nM - Return to Main Menu"
+                + "\n---------------------------------------------";
+
     }
-    
-     
+
     public void displayGameMenuView() {
-        
-        
-        
+
         boolean done = false; //set flag to not done
         do {
-            
+
             System.out.println(menu);
             String menuOption = this.getMenuOption();
-            if (menuOption.toUpperCase().equals ("Q")) //user wants to quit
+            if (menuOption.toUpperCase().equals("Q")) //user wants to quit
+            {
                 return; //exit the game
-        
+            }
             //do requested action and display next view
             done = this.doAction(menuOption);
-           
+
         } while (!done);
-             
-    }    
-        
+
+    }
 
     private String getMenuOption() {
-        
+
         Scanner keyboard = new Scanner(System.in); //get infile for keyboard
         String value = ""; //value to be returned
         boolean valid = false; //initialize to not valid
-        
+
         while (!valid) { //loop while an invalid value is entered
             //System.out.println("\n" + this.promptMessage);
-            
+
             value = keyboard.nextLine(); //get next line typed on keyboard
             value = value.trim(); //trim off leading and trailing blanks
-            
-            if (value.length() < 1){ //value is blank
+
+            if (value.length() < 1) { //value is blank
                 System.out.println("\nInvalid value. The value cannot be blank.");
                 continue;
             }
-            
+
             break; //end the loop
         }
-        
+
         return value; //return the value entered
     }
-    
 
     private boolean doAction(String choice) {
-        
+
         choice = choice.toUpperCase(); //convert choice to uppercase
-        
+
         switch (choice) {
             case "A": //display alien menu
                 this.AlienMenuView();
+                break;
+            case "L": //display location menu    
+                this.LocationView();
                 break;
             case "M": //returns player to Main Menu
                 return true;
             default:
                 System.out.println("\n*** Invalid selection. *** Please, try again!");
                 break;
-    }       
+        }
 
-    return false;
+        return false;
 
     }
 
+    private void AlienMenuView() {
 
-        private void AlienMenuView() {
-           
-          
-            AlienMenuView alienMenuView = new AlienMenuView();
-            alienMenuView.displayAlienMenuView();
-        
+        AlienMenuView alienMenuView = new AlienMenuView();
+        alienMenuView.displayAlienMenuView();
+
     }
-        
-                private void returnToMainMenu() {
-            MainMenuView mainMenu = new MainMenuView();
-            mainMenu.displayMainMenuView();
-        
+
+    private void returnToMainMenu() {
+        MainMenuView mainMenu = new MainMenuView();
+        mainMenu.displayMainMenuView();
+
+    }
+
+    private void LocationView() {
+        LocationView location = new LocationView();
+        location.displayLocationView();
     }
 
     private static class alienMenuView {
@@ -120,5 +123,5 @@ public class GameMenuView {
         public alienMenuView() {
         }
     }
-  
+
 }
---
+++ ReturnToRexburg1/src/byui/cit260/returnToRexburg/view/LocationView.java
@@ -0,0 +1,138 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package byui.cit260.returnToRexburg.view;
+
+/**
+ *
+ * @author douglasarbon1
+ */
+
+import java.util.Scanner;
+/**
+ *
+ * @author douglasarbon1
+ */
+public class LocationView {
+    
+    //class instance variable
+    String menu;
+
+    //default constructor initialized with text to be displayed
+    public LocationView() {
+        this.menu = "\n"
+                  + "\n---------------------------------------------"
+                  + "\n| Location Menu                                 |"
+                  + "\n---------------------------------------------"
+                  + "\nE - Earn a new trophy"
+                  + "\nD - Drill for fuel"
+                  + "\nF - View fuel percentage"
+                  + "\nT - View trophy count"
+                  + "\nM - Return to main menu"
+                  + "\n---------------------------------------------";      
+    }
+   
+    void displayMenu() {
+        System.out.println(menu);
+    }
+   
+    public void displayLocationView() {
+       
+        boolean done = false; //set flag to not done
+        do {
+            
+            System.out.println(menu);
+            String menuOption = this.getMenuOption();
+            if (menuOption.toUpperCase().equals ("Q")) //user wants to quit
+                return; //exit the game
+        
+            //do requested action and display next view
+            done = this.doAction(menuOption);
+           
+        } while (!done);
+             
+    }    
+   
+    private String getMenuOption() {
+        
+        Scanner keyboard = new Scanner(System.in); //get infile for keyboard
+        String value = ""; //value to be returned
+        boolean valid = false; //initialize to not valid
+        
+        while (!valid) { //loop while an invalid value is entered
+            //System.out.println("\n" + this.promptMessage);
+            
+            value = keyboard.nextLine(); //get next line typed on keyboard
+            value = value.trim(); //trim off leading and trailing blanks
+            
+            if (value.length() < 1 ){ //value is incorrect
+                System.out.println("\nInvalid value. The value is incorrect.");
+                continue;
+            }
+            
+            break; //end the loop
+        }
+        return value; //return the value entered
+    }
+    
+
+    public boolean doAction(String choice) {
+
+    choice = choice.toUpperCase(); //convert choice to uppercase
+
+    switch (choice) {
+        case "E": //displays question to earn a new trophy scene
+            this.viewTrophyQuestion();
+            break;
+        case "D": //display gather fuel scene
+            this.viewDrillForFuel();
+            break;
+        case "F": //display current fuel level scene
+            this.viewFuelLevel();
+            break;
+        case "T": //display current trophy count scene
+            this.viewTrophyCount();
+            break;        
+        case "M": //returns player to Main Menu
+            return true; 
+        default:
+            System.out.println("\n*** Invalid selection. *** Please, try again!");
+            break;
+    }       
+
+        return false;
+
+    }
+
+        //Stub functions and returnToMainMenu() called
+        private void viewTrophyQuestion() {
+        System.out.println("\n This calls earn trophy view.");
+        }
+        
+        private void viewDrillForFuel() {
+            System.out.println("\n This calls drill for fuel view.");
+
+            //DrillForFuelView drillForFuel = new DrillForFuelView();
+            //drillForFuel.displayDrillForFuelView();
+        }
+
+        private void viewFuelLevel() {
+        System.out.println("\n This calls the fuel level view.");
+        }
+        
+        private void viewTrophyCount() {
+        System.out.println("\n This calls the trophy room view.");
+        }
+      
+        private void returnToMainMenu() {
+            MainMenuView mainMenu = new MainMenuView();
+            mainMenu.displayMainMenuView();     
+    }
+     
+}
+
+
+
+
